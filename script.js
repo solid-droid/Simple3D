10769cea48739846ab65425da9a2759134b00dbf
Nikhil M Jeby <32586986+solid-droid@users.noreply.github.com>
2021-09-27 02:24:52 +0530

    3D model support + create Block method

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
+var clock = new THREE.Clock();
+var mixers = [];
+//////////////////////////////////////
+
 let engine = new simple3D({
-    onModelLoadComplete: () => {  }
+  onModelLoadComplete: () => { beginModelCreate(); },
+});
+
+const beginModelCreate = () => {
+  engine.createModel('M1','scene1',mixers,'mcdonalds', {
+    position: { x: -100, y: 15, z: -60 },
+    rotation: { x: 0, y: Math.PI/4, z: 0 },
+    scale:{x:0.02,y:0.02,z:0.02}
   });
+  engine.createModel('M2','scene1',mixers,'mcdonalds', {
+    position: { x: 55, y: 15, z: 0 },
+    rotation: { x: 0, y: -Math.PI/4, z: 0 },
+    scale:{x:0.02,y:0.02,z:0.02}
+  });
+  engine.createModel('M3','scene1',mixers,'mcdonalds', {
+    position: { x: 25, y: 15, z: -125 },
+    rotation: { x: 0, y: -0.3, z: 0 },
+    scale:{x:0.02,y:0.02,z:0.02}
+  });
+  engine.createModel('M4','scene1',mixers,'vendingMachine', {
+    position: { x: -33, y: 0, z: -42 },
+    rotation: { x: 0, y:-Math.PI/2, z: 0 },
+    scale:{x:1.5,y:1.5,z:1.5}
+  });
+  engine.createModel('M5','scene1',mixers,'arrowStraight', {
+    position: { x: -10, y: 5, z: -25 },
+    rotation: { x: 0, y: -Math.PI/4, z: 0 },
+    scale:{x:1,y:1,z:1}
+  });
+  engine.createModel('M6','scene1',mixers,'arrowStraight', {
+    position: { x: -20, y: 5, z: 0 },
+    rotation: { x: 0, y: 0, z: 0 },
+    scale:{x:1,y:1,z:1}
+  });
+  engine.createModel('M7','scene1',mixers,'arrowStraight', {
+    position: { x: -20, y: 5, z: 30 },
+    rotation: { x: 0, y: 0, z: 0 },
+    scale:{x:1,y:1,z:1}
+  });
+  engine.createModel('M8','scene1',mixers,'arrowStraight', {
+    position: { x: -10, y: 5, z: 50 },
+    rotation: { x: 0, y: 0.8, z: 0 },
+    scale:{x:1,y:1,z:1}
+  });
+};
+
+let domEvents = engine.getDomEvents();
+////////////////////////////////////
 
 let renderer = engine.createRenderer("canvas");
 engine.createScene('scene1');
@@ -10,17 +60,17 @@ engine.setCameraPosition('camera1', 'scene1');
 engine.setActive('scene1', 'camera1', 'control1');
 
 [
- {path: './3dModels/robot1/scene.gltf', name: 'robot1'},
- {path: './3dModels/car1/scene.gltf' , name: 'car'},
+ {path: './3dModels/mcdonalds/scene.gltf', name: 'mcdonalds'},
+ {path: './3dModels/arrow-straight/scene.gltf' , name: 'arrowStraight'},
+ {path: './3dModels/vendingMachine/scene.gltf' , name: 'vendingMachine'},
 ].forEach(x => engine.loadModel(x.name, x.path));
 
-engine.createLight('light1','scene1');
+engine.createLight('ambient','scene1');
+engine.createDirectionalLight('directional','scene1', '#b1b1b1', {x : 15 ,y: 4 , z: 10});
+engine.createGround('scene1');
 
-let domEvents = engine.getDomEvents();
+engine.createBlocksFromArray('scene1',mapData.data.buildings, {color: '#868686'});
 
-var clock = new THREE.Clock();
-
-var mixers = [];
 
 
 function render() {
@@ -28,6 +78,14 @@ function render() {
   var delta = clock.getDelta();
   mixers.forEach(mixer => mixer.update( delta ));
   engine.activeControl.update();
+  console.log();
+  const M1 = engine.getActiveModel('M1');
+  const M2 = engine.getActiveModel('M2');
+  const M3 = engine.getActiveModel('M3');
+  if(M1) engine.getActiveModel('M1').scene.rotation.y+= 0.005;
+  if(M2) engine.getActiveModel('M2').scene.rotation.y+= 0.005;
+  if(M3) engine.getActiveModel('M3').scene.rotation.y+= 0.005;
+
   renderer.render(engine.activeScene, engine.activeCamera);
 }
 
--- simple3D.js
+++ simple3D.js
@@ -10,6 +10,7 @@ class simple3D{
 
     //resourses
     modelList = {};
+    activeModel = {};
     controlList = {};
     sceneList = {};
     cameraList = {};
@@ -56,7 +57,7 @@ class simple3D{
         this.sceneList[name] = scene;
     }
 
-    createCamera(name , angle = 50, min = 0.1 , max = 100000) {
+    createCamera(name , angle = 45, min = 10 , max = 10000) {
         const camera = new THREE.PerspectiveCamera( angle, window.innerWidth / window.innerHeight, min , max);
         this.cameraList[name] = camera;
         this.setCamera(name);
@@ -70,19 +71,19 @@ class simple3D{
             ZOOM: THREE.MOUSE.MIDDLE,
             PAN: THREE.MOUSE.LEFT
         };
-        controls.minDistance  =2000;
-        controls.maxDistance  = 10000;
+        controls.minDistance  =50;
+        controls.maxDistance  = 300;
         controls.enableDamping = true; 
         controls.dampingFactor = 0.12;  
         controls.rotateSpeed = 0.08; 
         controls.autoRotate = false;
         controls.autoRotateSpeed = 0.08;
-        controls.maxPolarAngle = Math.PI/2;
+        controls.maxPolarAngle = Math.PI/2-0.1;
         this.controlList[name] = controls;
         return controls;
     }
 
-    createAmbientLight(name, scene, color =  0x919191 ){
+    createAmbientLight(name, scene, color =  '#e0e4e4' ){
         const light = new THREE.AmbientLight( color );
         (this.getScene(scene)).add( light )
         this.lightList[name] = {scene, light};
@@ -95,7 +96,15 @@ class simple3D{
             this.lightList[name] = {scene, light};
     }
 
-    createLight(name, scene, color =  0x919191, position = undefined){
+    createSpotLight = (name, scene, color =  'red', position = {x : 5 ,y: 1 , z: 1}) =>{
+
+        const light = new THREE.SpotLight( color,10,70,Math.PI/2,1,2);
+        light.position.set( position.x , position.y , position.z );
+        this.getScene(scene).add( light )
+        this.lightList[name] = {scene, light};
+    }
+
+    createLight(name, scene, color =  '#e0e4e4', position = undefined){
         if(position){
             this.createDirectionalLight(name, scene, color , position);
         } else {
@@ -142,8 +151,8 @@ class simple3D{
 
     setCameraPosition(camera, scene , {position ,  rotate, pivot }
         = {
-            position : {x : 6000 ,y: 3000, z: 3000},
-            rotate   : {x : 0, y: 0.465 , z: 0.05},
+            position : {x : 30 ,y: 50, z: 90},
+            rotate   : {x : 0, y: -0.3 , z: 0.05},
             pivot    : {x : 0, y: 0, z: 0}
         }
         ){
@@ -170,4 +179,139 @@ class simple3D{
         scene.add( camera_pivot );
         camera.position.set( position.x, position.y, position.z);
     }
+
+    ///////////////
+    createGround = (scene) => {
+
+        var geo = new THREE.PlaneBufferGeometry(1000, 1000);
+        var mat = new THREE.MeshBasicMaterial({ color: '#b7b7b7', side: THREE.DoubleSide });
+        var plane = new THREE.Mesh(geo, mat);
+        plane.rotateX( - Math.PI / 2);
+        plane.position.x = 0;
+        plane.position.z = 0;
+        plane.position.y = 0;
+
+        if(typeof scene == 'string'){
+            scene = this.getScene(scene);
+        }
+
+        scene.add(plane);
+    }
+
+    //////////////////////////
+    createBlock = (scene = null, config ) => {
+        const {x,y,z} = config.position || {x : 0, y: 0, z: 0};
+        const size = config.scale || {x : 1, y: 1, z: 1};
+        const color = config.color || 'red';
+        const rotation = config.rotation || {x : 0, y: 0, z: 0};
+
+
+        const offset = { x : -50 , y:0 , z: -50};
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshPhongMaterial( { color , flatShading: true } );
+        const block = new THREE.Mesh( geometry, material );
+        block.position.x = x + offset.x;
+        block.position.y = y + offset.y;
+        block.position.z = z - offset.z;
+        block.scale.x = size.x;
+        block.scale.y = size.y;
+        block.scale.z = size.z;
+        if(typeof scene == 'string'){
+            scene = this.getScene(scene);
+        }
+        if(scene){
+            scene.add(block);
+        }
+        return block;
+    };
+
+    createBlocksFromArray = (scene, array, config= {}) => {
+        const color = config.color;
+
+        array.forEach(item => {
+            this.createBlock(scene,{
+                position: item.position,
+                scale: item.scale,
+                color: color ? color : item.color,
+                });
+        });
+    }
+
+    createModel = (name, scene, mixers, modelName, config = {}) => {
+        const {x,y,z} = config.position || {x : 0, y: 0, z: 0};
+        const rot = config.rotation || {x : 0, y: 0, z: 0};
+        const size = config.scale || {x : 1, y: 1, z: 1};
+        const model = this.cloneGltf(this.getModel(modelName));
+        const animation = config.animation || false;
+        model.scene.position.set(x, y, z);
+        model.scene.scale.x = size.x;
+        model.scene.scale.y = size.y;
+        model.scene.scale.z = size.z;
+        model.scene.rotateX(rot.x);
+        model.scene.rotateY(rot.y);
+        model.scene.rotateZ(rot.z);
+        this.activeModel[name] = model;
+        if(typeof scene == 'string'){
+            scene = this.getScene(scene);
+        }
+        scene.add( model.scene );
+        if(animation){
+            const mixer = new THREE.AnimationMixer( model.scene );
+            mixers.push(mixer);
+            const action = mixer.clipAction( model.animations[ 0 ] ); // access first animation clip
+            action.play();
+        }
+    }
+    
+    getActiveModel = (name) => {
+        const model = this.activeModel[name];
+        return model;
+    };
+    cloneGltf = (gltf) => {
+        !gltf && console.error('Model not found');
+        const clone = {
+          animations: gltf.animations,
+          scene: gltf.scene.clone(true)
+        };
+      
+        const skinnedMeshes = {};
+      
+        gltf.scene.traverse(node => {
+          if (node.isSkinnedMesh) {
+            skinnedMeshes[node.name] = node;
+          }
+        });
+      
+        const cloneBones = {};
+        const cloneSkinnedMeshes = {};
+      
+        clone.scene.traverse(node => {
+          if (node.isBone) {
+            cloneBones[node.name] = node;
+          }
+      
+          if (node.isSkinnedMesh) {
+            cloneSkinnedMeshes[node.name] = node;
+          }
+        });
+      
+        for (let name in skinnedMeshes) {
+          const skinnedMesh = skinnedMeshes[name];
+          const skeleton = skinnedMesh.skeleton;
+          const cloneSkinnedMesh = cloneSkinnedMeshes[name];
+      
+          const orderedCloneBones = [];
+      
+          for (let i = 0; i < skeleton.bones.length; ++i) {
+            const cloneBone = cloneBones[skeleton.bones[i].name];
+            orderedCloneBones.push(cloneBone);
+          }
+      
+          cloneSkinnedMesh.bind(
+              new Skeleton(orderedCloneBones, skeleton.boneInverses),
+              cloneSkinnedMesh.matrixWorld);
+        }
+      
+        return clone;
+      }
 }
